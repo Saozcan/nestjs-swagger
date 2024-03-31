c93f5f94d6994d0c4baf3f6e8ab84072937221e4
Samim <samim.ozcan@atez.com.tr>
2024-03-31 07:01:04 +0300

    add swagger-service

--- src/metadata.ts
+++ src/metadata.ts
@@ -1,42 +1,7 @@
 /* eslint-disable */
 export default async () => {
-  const t = {
-    ['./app.dto']: await import('./app.dto'),
-  };
-  return {
-    '@nestjs/swagger': {
-      models: [
-        [
-          import('./app.dto'),
-          {
-            CreateUserDto: {
-              email: { required: true, type: () => String },
-              password: { required: true, type: () => String },
-              roles: {
-                required: true,
-                enum: t['./app.dto'].RoleEnum,
-                isArray: true,
-              },
-              isEnabled: {
-                required: false,
-                type: () => Boolean,
-                default: true,
-              },
-            },
-          },
-        ],
-      ],
-      controllers: [
-        [
-          import('./app.controller'),
-          {
-            AppController: {
-              getHello: { type: String },
-              createUser: { type: String },
-            },
-          },
-        ],
-      ],
-    },
-  };
+    const t = {
+        ["./app.dto"]: await import("./app.dto")
+    };
+    return { "@nestjs/swagger": { "models": [[import("./app.dto"), { "CreateUserDto": { email: { required: true, type: () => String }, password: { required: true, type: () => String }, roles: { required: true, enum: t["./app.dto"].RoleEnum, isArray: true }, isEnabled: { required: false, type: () => Boolean, default: true } } }]], "controllers": [[import("./app.controller"), { "AppController": { "getHello": { type: String }, "createUser": { type: String } } }]] } };
 };
